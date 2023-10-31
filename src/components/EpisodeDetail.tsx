1febafe78e0bce6d207ac78cb8c7f5a79ce958d6
andreea21ungureanu <andreea.ungureanu221@gmail.com>
2023-10-31 15:07:03 +0000

    create app in next js

---
+++ src/components/EpisodeDetail.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+
+const EpisodeDetailContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex: 1;
+`;
+
+export const EpisodeDetail = () => {
+  return (
+    <EpisodeDetailContainer>
+      Pick an episode from the left
+    </EpisodeDetailContainer>
+  );
+};
---
+++ src/components/EpisodeList.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledEpisodeList = styled.ol`
+  margin: 0;
+  overflow: scroll;
+  flex: 1;
+
+  list-style-type: none;
+  padding-inline-start: 0;
+`;
+
+const EpisodeTitle = styled.li`
+  font-size: 18px;
+
+  margin-bottom: 16px;
+`;
+type EpisodeList = {
+  episodes: Episode[];
+};
+export const EpisodeList = ({ episodes = [] }: EpisodeList) => {
+  return (
+    <StyledEpisodeList>
+      {episodes.map((ep) => (
+        <EpisodeTitle key={ep.id}>
+          <span>
+            S{ep.season}E{ep.number}:
+          </span>{" "}
+          {ep.name}
+        </EpisodeTitle>
+      ))}
+    </StyledEpisodeList>
+  );
+};
